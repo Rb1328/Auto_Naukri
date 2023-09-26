5f600b3b7f19dea35473016b6caa1efcee185573
Rb1328 <bhavarrahul1328@gmail.com>
2023-09-26 12:33:45 +0530

    Folder name change

--- src/test/java/com/naukri/testCases/BaseClass.java
+++ src/test/java/com/naukri/testCases/BaseClass.java
@@ -26,7 +26,7 @@ public class BaseClass  extends ExcelData{
 
 	@BeforeTest
 	public static void launchChrome() {
-		System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"//Drivers//msedgedriver.exe");
+		System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"//Driver//msedgedriver.exe");
 		 driver=new EdgeDriver();
 		 
 		
